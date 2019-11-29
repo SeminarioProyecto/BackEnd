df7a8022ba0d6f6668abca48b4b918378ef5ad28
athroscf <fialloschris1@gmail.com>
2019-11-28 23:12:56 -0600

    Reversing changes.

--- config/axios.js
+++
@@ -1,7 +0,0 @@
-const axios = require("axios");
-
-const axiosClient = axios.create({
-    baseUrl: "http://data.fixer.io/api"
-});
-
-exports.axiosClient;
--- index.js
+++ index.js
@@ -1,17 +1,2 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const routes = require("./routes/index");
 
-// Crear servidor
-const app = express();
-
-// Habilitar body parser
-app.use(bodyParser.json);
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use("/", routes());
-
-app.listen(8000, () => {
-    console.log("Listening on PORT 8000");
-});
